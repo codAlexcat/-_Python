8432427de10ad1324d491948c7b72f1b4af0158a
Alex_Graf_cod <grafalex2005@gmail.com>
2025-12-21 20:37:31 +0800

    Add files via upload

---
+++ studens.py
@@ -0,0 +1,14 @@
+
+math_students = {"Анна", "Борис", "Виктор", "Дарья", "Елена"}
+physics_students = {"Виктор", "Георгий", "Дарья", "Иван", "Ксения"}
+cs_students = {"Анна", "Виктор", "Елена", "Иван", "Мария"}
+
+third_year = math_students & physics_students & cs_students
+print(f"Во всех трех множествах: {third_year}")
+
+one_math = math_students - physics_students - cs_students
+print(f"Посещали только математику: {one_math}")
+
+two = (math_students & physics_students) | (math_students & cs_students) | (physics_students & cs_students)
+print(f"Те кто есть только в двух множествах: {two}")
+
